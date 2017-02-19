f1ab6a4ad36cdffb38d7fb8f074f9e590aa6a8ea
milena <mile.card195@gmail.com>
2017-02-19 15:46:47 -0500

    Diseño de interfaz  ContacInfo

--- LabsCM20171/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20171/lab1/ContactInfo.java
+++ LabsCM20171/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20171/lab1/ContactInfo.java
@@ -2,6 +2,9 @@ package co.edu.udea.compumovil.gr08_20171.lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 
 public class ContactInfo extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class ContactInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_info);
+
+        // Get a reference to the AutoCompleteTextView in the layout
+        AutoCompleteTextView TextViewPais = (AutoCompleteTextView) findViewById(R.id.AutoCompletePaises);
+
+        // Get the string array
+        String[] paises = getResources().getStringArray(R.array.paises_arrays);
+
+        // Create the adapter and set it to the AutoCompleteTextView
+        ArrayAdapter<String> adapterPais =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, paises);
+        TextViewPais.setAdapter(adapterPais);
+
+        // Get a reference to the AutoCompleteTextView in the layout
+        AutoCompleteTextView TextViewCiudad = (AutoCompleteTextView) findViewById(R.id.AutoCompleteCiudades);
+
+        // Get the string array
+        String[] ciudades = getResources().getStringArray(R.array.ciudades_arrays);
+
+        // Create the adapter and set it to the AutoCompleteTextView
+        ArrayAdapter<String> adapterCiudad =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ciudades);
+        TextViewCiudad.setAdapter(adapterCiudad);
     }
 }
