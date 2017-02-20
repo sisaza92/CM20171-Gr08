6f584710e08e5a775e919c95091354c413403606
sisaza92 <sisaz92@gmail.com>
2017-02-19 22:20:54 -0500

    Se agregaron los datos en el Bundle y se pasaron a la actividad OtherInfo que es la próxima en lanzarse. Quedó pendiente pasar la fecha.

--- LabsCM20171/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20171/lab1/activity/PersonalInfo.java
+++ LabsCM20171/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20171/lab1/activity/PersonalInfo.java
@@ -1,6 +1,7 @@
 package co.edu.udea.compumovil.gr08_20171.lab1.activity;
 
 import android.app.DialogFragment;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +13,12 @@ import android.widget.Spinner;
 import co.edu.udea.compumovil.gr08_20171.lab1.util.DatePickerFragment;
 import co.edu.udea.compumovil.gr08_20171.lab1.R;
 
-
+/**
+ * @author Cristian Camilo Isaza
+ * @email sisaza92@gmail.com
+ * @date 17/02/2017
+ * Esta clase Controla la vista de la actividad PersonalInfo.
+ */
 public class PersonalInfo extends AppCompatActivity{
 
     EditText input_nombres,input_apellidos;
@@ -26,15 +32,27 @@ public class PersonalInfo extends AppCompatActivity{
 
         input_nombres = (EditText) findViewById(R.id.input_nombres);
         input_apellidos = (EditText) findViewById(R.id.input_apellidos);
-        RadioGroup rgSex = (RadioGroup) findViewById(R.id.rgSexo);
+        rgSexo = (RadioGroup) findViewById(R.id.rgSexo);
         sprGradoEscolaridad = (Spinner) findViewById(R.id.sprGradoEscolaridad);
     }
 
+    /**
+     * Método creado para mostrar el diálogo con el
+     * selector de fecha y que es accionado por el boton
+     * cambiar desde el XML.
+     * @param view parámetro requerido para llamar el método a travéz del onClick en el XML.
+     */
     public void showDatePicker(View view){
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getFragmentManager(), "timePicker");
     }
 
+    /**
+     * Método ejecutado por el boton siguiente de la actividad y
+     * que se encarga de recopilar los datos y lanzar la proxima
+     * actividad.
+     * @param view parámetro requerido para llamar el método a travéz del onClick en el XML.
+     */
     public void showNextActivity(View view){
 
         Bundle datos = new Bundle();
@@ -44,7 +62,12 @@ public class PersonalInfo extends AppCompatActivity{
 
         RadioButton rbChecked = (RadioButton) findViewById(rgSexo.getCheckedRadioButtonId());
         datos.putString("sexo", rbChecked.getText().toString());
+        //TODO: Agregar la fecha al Bundle.
         datos.putString("grado", sprGradoEscolaridad.getSelectedItem().toString());
+
+        Intent otherInfo = new Intent(view.getContext(),ContactInfo.class);
+        otherInfo.putExtra("datos",datos);
+        startActivity(otherInfo);
 
     }
 
