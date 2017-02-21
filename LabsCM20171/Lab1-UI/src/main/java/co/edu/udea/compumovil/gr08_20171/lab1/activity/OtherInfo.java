91637b5298587b5e2432cdcb7a31c97803564615
sisaza92 <sisaz92@gmail.com>
2017-02-20 21:55:49 -0500

    Se agregó funcionalidad al botón y un textview para mostrar el resumen de la información recolectada.

--- LabsCM20171/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20171/lab1/activity/OtherInfo.java
+++ LabsCM20171/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20171/lab1/activity/OtherInfo.java
@@ -2,14 +2,28 @@ package co.edu.udea.compumovil.gr08_20171.lab1.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import co.edu.udea.compumovil.gr08_20171.lab1.R;
 
 public class OtherInfo extends AppCompatActivity {
 
+    TextView tvResumen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_info);
+
+        tvResumen = (TextView)findViewById(R.id.tvResumen);
+    }
+
+    public void mostrarInformacion(View view){
+
+        Bundle datos = getIntent().getBundleExtra("datos");
+
+        tvResumen.setText(datos.toString());
+
     }
 }
