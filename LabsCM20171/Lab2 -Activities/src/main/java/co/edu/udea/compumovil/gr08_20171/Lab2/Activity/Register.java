e2c9b7f406b27f1d55003c7058eec58ccf990792
sisaza92 <sisaz92@gmail.com>
2017-02-23 22:45:04 -0500

    Se Creo el Prroyecto inicial para el Laboratorio #2

---
+++ LabsCM20171/Lab2 -Activities/src/main/java/co/edu/udea/compumovil/gr08_20171/Lab2/Activity/Register.java
@@ -0,0 +1,15 @@
+package co.edu.udea.compumovil.gr08_20171.Lab2.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.edu.udea.compumovil.gr08_20171.Lab2.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
